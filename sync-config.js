64524db5ad1eafaa6f323612a7a6ddb9d9e57ba0
李老師 <yrhc1255@users.noreply.github.com>
2026-09-20 20:35:48 +0800

    Connect public course to GAS score sync

--- sync-config.js
+++ sync-config.js
@@ -1,3 +1,3 @@
-export const gasEndpoint='';
+export const gasEndpoint='https://script.google.com/macros/s/AKfycbypwIxx63lAS_lu1t44-t-33Anv6sWFOu3BzUZZw0l4fD677XZ2Uil9iFOyyghFyBziig/exec';
 export const courseId='food-nutrients-energy';
 export const cohortId='115-1';
